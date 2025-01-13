dc8a682a07de95e85d71602a5ff3829ce1434402
ratman528 <adevries528@gmail.com>
2025-01-12 20:40:04 -0600

    initial coi

---
+++ src/main.c
@@ -0,0 +1,129 @@
+#include "mprintf.h"
+
+#include "stm32wlxx_ll_bus.h"
+#include "stm32wlxx_ll_rcc.h"
+#include "stm32wlxx_ll_gpio.h"
+#include "stm32wlxx_ll_lpuart.h"
+#include "stm32wlxx_ll_utils.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <string.h>
+
+static void UART_init(void);
+static void sysclk_init(void);
+static void Error_Handler(void);
+
+static uint8_t buffer[256] = {0};
+
+
+int main(void)
+{
+  sysclk_init();
+  UART_init();
+
+  #define ENTRY_LEN 10
+
+  for(int i=0; i < ENTRY_LEN; i++){
+    buffer[0x10 + i] = i;
+  }
+
+  memmove(&buffer[0], &buffer[0x10], ENTRY_LEN);
+
+
+  println_("hello world!");
+
+  while (1)
+  {
+    
+  	LL_mDelay(1000);
+
+  }
+}
+
+
+int32_t putchar_(char c)
+{
+  // loop while the LPUART_TDR register is full
+  while(LL_LPUART_IsActiveFlag_TXE_TXFNF(LPUART1) != 1);
+  // once the LPUART_TDR register is empty, fill it with char c
+  LL_LPUART_TransmitData8(LPUART1, (uint8_t)c);
+  return (c);
+}
+
+static void sysclk_init(void)
+{
+  // update the global variable SystemCoreClock
+  SystemCoreClockUpdate();
+
+  // configure 1ms systick for easy delays
+  LL_RCC_ClocksTypeDef clk_struct;
+  LL_RCC_GetSystemClocksFreq(&clk_struct);
+  LL_Init1msTick(clk_struct.HCLK1_Frequency);
+}
+
+static void UART_init(void)
+{
+  // enable the UART GPIO port clock
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+
+  // set the LPUART clock source to the peripheral clock
+  LL_RCC_SetLPUARTClockSource(LL_RCC_LPUART1_CLKSOURCE_PCLK1);
+
+  // enable clock for LPUART
+  LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_LPUART1);
+
+  // configure GPIO pins for LPUART1 communication
+  // TX Pin is PA2, RX Pin is PA3
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {
+  .Pin = LL_GPIO_PIN_2 | LL_GPIO_PIN_3,
+  .Mode = LL_GPIO_MODE_ALTERNATE,
+  .Pull = LL_GPIO_PULL_NO,
+  .Speed = LL_GPIO_SPEED_FREQ_MEDIUM,
+  .OutputType = LL_GPIO_OUTPUT_PUSHPULL,
+  .Alternate = LL_GPIO_AF_8
+};
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  // configure the LPUART to transmit with the following settings:
+  // baud = 115200, data bits = 8, stop bits = 1, parity bits = 0
+  LL_LPUART_InitTypeDef LPUART_InitStruct = {
+      .PrescalerValue = LL_LPUART_PRESCALER_DIV1,
+      .BaudRate = 115200,
+      .DataWidth = LL_LPUART_DATAWIDTH_8B,
+      .StopBits = LL_LPUART_STOPBITS_1,
+      .Parity = LL_LPUART_PARITY_NONE,
+      .TransferDirection = LL_LPUART_DIRECTION_TX_RX,
+      .HardwareFlowControl = LL_LPUART_HWCONTROL_NONE
+  };
+  LL_LPUART_Init(LPUART1, &LPUART_InitStruct);
+  LL_LPUART_Enable(LPUART1);
+
+  // wait for the LPUART module to send an idle frame and finish initialization
+  while(!(LL_LPUART_IsActiveFlag_TEACK(LPUART1)) || !(LL_LPUART_IsActiveFlag_REACK(LPUART1)));
+}
+
+
+static void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+}
+#endif /* USE_FULL_ASSERT */
