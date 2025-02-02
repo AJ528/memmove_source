6d3c9fb56f41ec78883b2cce0c0e2a8f5f1bec52
ratman528 <adevries528@gmail.com>
2025-02-01 19:43:53 -0600

    added more tests

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 
 #include "stm32wlxx_ll_bus.h"
 #include "stm32wlxx_ll_rcc.h"
+#include "stm32wlxx_ll_system.h"
+#include "stm32wlxx_ll_pwr.h"
 #include "stm32wlxx_ll_gpio.h"
 #include "stm32wlxx_ll_lpuart.h"
 #include "stm32wlxx_ll_utils.h"
@@ -24,8 +26,6 @@ static void sysclk_init(void);
 #define DEST_OFFSET 0x41
 
 
-extern void* memset_orig(void *ptr, uint32_t value, uint32_t num);
-extern void* memmove_orig(void *destination, const void *source, size_t num);
 extern void* memmove_new(void *destination, const void *source, size_t num);
 
 static void test(void* (*f)(void *, const void *, size_t), char * func_name);
@@ -38,32 +38,46 @@ static void set_buffer(uint8_t *buf);
 extern void initialise_monitor_handles(void);
 
 
-TEST memmove_test(uint32_t data_len, uint32_t src_offset, uint32_t dest_offset)
+TEST memmove_test(uint32_t data_len, uint32_t src_offset, uint32_t dest_offset, bool print_performance)
 {
-    uint8_t expected[BUFFER_SIZE] = {0};
-    uint8_t actual[BUFFER_SIZE] = {0};
 
-    if(((data_len + src_offset) >= BUFFER_SIZE) || ((data_len + dest_offset) >= BUFFER_SIZE)){
-      FAIL();
-    }
+  uint8_t expected[BUFFER_SIZE] = {0};
+  uint8_t actual[BUFFER_SIZE] = {0};
+
+  if(((data_len + src_offset) >= BUFFER_SIZE) || ((data_len + dest_offset) >= BUFFER_SIZE)){
+    FAIL();
+  }
 
 
-    uint32_t i;
+  uint32_t i;
 
-    for(i = 0; i < data_len; i++){
-      expected[src_offset + i] = i;
-      actual[src_offset + i] = i;
-    }
+  for(i = 0; i < data_len; i++){
+    expected[src_offset + i] = i;
+    actual[src_offset + i] = i;
+  }
 
-    memmove(&(expected[dest_offset]), &(expected[src_offset]), data_len);
-    memmove_new(&(actual[dest_offset]), &(actual[src_offset]), data_len);
+  uint32_t memmove_orig_start = get_cycle_count();
+  memmove(&(expected[dest_offset]), &(expected[src_offset]), data_len);
+  uint32_t memmove_orig_stop = get_cycle_count();
 
-    // force fail
-    // actual[dest_offset+16] = 0;
+  uint32_t memmove_new_start = get_cycle_count();
+  memmove_new(&(actual[dest_offset]), &(actual[src_offset]), data_len);
+  uint32_t memmove_new_stop = get_cycle_count();
 
-    ASSERT_MEM_EQ(expected, actual, BUFFER_SIZE);
 
-    PASS();
+
+  if(print_performance){
+    uint32_t orig_cycle = memmove_orig_stop-memmove_orig_start;
+    uint32_t new_cycle = memmove_new_stop-memmove_new_start;
+    printfln_("%-10u %-10u %-10u %-12u %-12u", data_len, src_offset, dest_offset, orig_cycle, new_cycle);
+  }
+
+  // memmove(&(expected[dest_offset]), &(expected[src_offset]), data_len);
+  // memmove_new(&(actual[dest_offset]), &(actual[src_offset]), data_len);
+
+  ASSERT_MEM_EQ(expected, actual, BUFFER_SIZE);
+
+  PASS();
 }
 
 TEST memmove_iterate(uint32_t data_len_limit)
@@ -73,11 +87,18 @@ TEST memmove_iterate(uint32_t data_len_limit)
   uint32_t dest_offset;
   uint32_t offset_limit;
 
+  bool print_performance = false;
+
   for(data_len = 0; data_len < data_len_limit; data_len++){
     offset_limit = (data_len * 2)+5;
+    if(data_len == data_len_limit - 1){
+      print_performance = true;
+    }else{
+      print_performance = false;
+    }
     for(src_offset = 0; src_offset < offset_limit; src_offset++){
       for(dest_offset = 0; dest_offset < offset_limit; dest_offset++){
-        CHECK_CALL(memmove_test(data_len, src_offset, dest_offset));
+        CHECK_CALL(memmove_test(data_len, src_offset, dest_offset, false));
       }
     }
   }
@@ -91,21 +112,44 @@ GREATEST_MAIN_DEFS();
 int main(void)
 {
   initialise_monitor_handles();
-
   sysclk_init();
   UART_init();
-
   enable_cycle_count();
 
-  test(memmove_new, "memmove_new");
-  test(memmove_orig, "memmove_orig");
-  test(memmove, "lib_memmove");
-  print_newline();
+  printfln_("%-10s %-10s %-10s %-12s %-12s", "data_len", "src_off", "dest_off", "orig_cycle", "new_cycle");
+
+
+  // test(memmove_new, "memmove_new");
+  // test(memmove, "lib_memmove");
+  // print_newline();
 
   GREATEST_MAIN_BEGIN();  // command-line options, initialization.
   
-  RUN_TESTp(memmove_test, 42, 5, 70);
-  RUN_TEST1(memmove_iterate, 32);
+  RUN_TESTp(memmove_test, 42, 5, 70, true);
+
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x1C, true);
+
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x1C, true);
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x1D, true);
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x1E, true);
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x1F, true);
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x20, true);
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x21, true);
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x22, true);
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x23, true);
+  RUN_TESTp(memmove_test, 0x100, 0x20, 0x24, true);
+
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x1D, true);
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x1E, true);
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x1F, true);
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x20, true);
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x21, true);
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x22, true);
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x23, true);
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x24, true);
+  RUN_TESTp(memmove_test, 0x100, 0x21, 0x25, true);
+
+  RUN_TEST1(memmove_iterate, 24);
 
   GREATEST_MAIN_END();    // display results
 
@@ -130,13 +174,6 @@ static void test(void* (*f)(void *, const void *, size_t), char * func_name)
   uint32_t LSU_stop = get_LSU_count();
 
   printfln_("%s took %u cycles and %u LSU cycles", func_name, stop-start, (uint8_t)(LSU_stop-LSU_start));
-}
-
-
-
-static void clear_buffer(uint8_t *buf, uint32_t size)
-{
-  memset_orig(buf, 0, size);
 }
 
 static void set_buffer(uint8_t *buf)
@@ -180,6 +217,37 @@ int32_t putchar_(char c)
 
 static void sysclk_init(void)
 {
+  //set up to run off the 48MHz MSI clock
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
+  while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2)
+  {
+  }
+
+  // Configure the main internal regulator output voltage
+  // set voltage scale to range 1, the high performance mode
+  // this sets the internal main regulator to 1.2V and SYSCLK can be up to 64MHz
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+  while(LL_PWR_IsActiveFlag_VOS() == 1); // delay until VOS flag is 0
+
+  // enable a wider range of MSI clock frequencies
+  LL_RCC_MSI_EnableRangeSelection();
+
+  /* Insure MSIRDY bit is set before writing MSIRANGE value */
+  while (LL_RCC_MSI_IsReady() == 0U)
+  {}
+
+  /* Set MSIRANGE default value */
+  LL_RCC_MSI_SetRange(LL_RCC_MSIRANGE_11);
+
+
+  // delay until MSI is ready
+  while (LL_RCC_MSI_IsReady() == 0U)
+  {}
+
+  // delay until MSI is system clock
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_MSI)
+  {}
+
   // update the global variable SystemCoreClock
   SystemCoreClockUpdate();
 
